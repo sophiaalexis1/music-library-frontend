8de39daaaf06614ccd84fb236745c8b1aea47812
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-20 17:44:52 -0400

    updated styling of table with boostrap

--- src/App.js
+++ src/App.js
@@ -3,29 +3,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBar from './Components/NavBar/NavBar';
 import MusicTable from './Components/MusicTable/MusicTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    getAllSongs();
-  },[]);
-  
-  async function getAllSongs() {
-      const response = await axios.get('http://127.0.0.1:8000/api/music/');
-      console.log(response.data);
-      setSongs(response.data)  
-  }
-
-  
   
   
   return (
     <div>
       <NavBar />
-      <button onClick={() => getAllSongs()}>Get All Songs </button>
-    <MusicTable />
+      <MusicTable />
     </div>
   );
 }
--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import './MusicTable.css';
+
+
 
 function MusicTable() {
   const [songs, setSongs] = useState([]);
@@ -17,7 +21,7 @@ function MusicTable() {
   return (
     <div>
       <h2>Music Library</h2>
-      <table>
+      <Table striped bordered hover variant="dark">
         <thead>
           <tr>
             <th>Title</th>
@@ -34,9 +38,10 @@ function MusicTable() {
               <td>{song.album}</td>
               <td>{song.release_date}</td>
             </tr>
+
           ))}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 }
